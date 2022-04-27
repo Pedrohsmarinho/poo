862e3b8104f3cf3b2a13cb4a43e8907e38166136
Pedro Marinho <pedrohsmarinho@outlook.com>
2022-04-26 22:22:27 -0300

    Aula referente ao dia 19-04-22

---
+++ Aula19-4/Assault/src/Officer.java
@@ -0,0 +1,14 @@
+public class Officer {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String arrestTheThief(Thief thief){
+        return "Not today";
+    }
+}
---
+++ Aula19-4/Assault/src/Victim.java
@@ -0,0 +1,11 @@
+public class Victim {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
